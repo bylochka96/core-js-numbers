cf19ecfd4923a5d85021682db5bf258f7aecc4c2
Efim Gutnik <bylochka96@gmail.com>
2024-11-11 19:40:02 +0300

    feat: tasks 10,11 done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -179,8 +179,8 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -200,8 +200,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) return false;
+  if (n === 3) return true;
+  if (n > 3 && (n % 2 === 0 || n % 3 === 0)) return false;
+  for (let i = 5; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
